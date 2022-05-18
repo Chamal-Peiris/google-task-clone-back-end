c48ba0d7edc95ff9a8f433ba6e26e46188599f34
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-18 18:56:46 +0530

    Finish handle exceprion part 1

---
+++ src/main/java/lk/ijse/dep8/tasks/util/HttpServlet2.java
@@ -0,0 +1,34 @@
+package lk.ijse.dep8.tasks.util;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.dep8.tasks.listener.LogInitializer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HttpServlet2 extends HttpServlet {
+    private  Logger logger = Logger.getLogger(LogInitializer.class.getName());
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            super.service(req, resp);
+        } catch (Throwable t) {
+            logger.logp(Level.SEVERE, t.getStackTrace()[0].getClassName(),
+                    t.getStackTrace()[0].getMethodName(), t.getMessage(), t);
+
+            resp.setContentType("application/json");
+            HttpResponseErrorMessage errorMsg = new  HttpResponseErrorMessage(new Date().getTime(),
+                    500, "Internal Server Error",
+                    t.getLocalizedMessage(), t.getMessage(), req.getRequestURI());
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(errorMsg, resp.getWriter());
+        }
+    }
+}
