917692e7789e379c20edb07a5e26c4b100ceab04
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-19 13:12:08 +0530

    Implement validation logic for add user

--- src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep8.tasks.api;
 
 import lk.ijse.dep8.tasks.util.HttpServlet2;
+import lk.ijse.dep8.tasks.util.ResponseStatusException;
 
 import javax.annotation.Resource;
 import javax.servlet.*;
@@ -13,6 +14,7 @@ import java.io.IOException;
 public class UserServlet extends HttpServlet2 {
     @Resource(name = "java:comp/env/jdbc/pool")
     private volatile DataSource pool;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -20,10 +22,24 @@ public class UserServlet extends HttpServlet2 {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    if(request.getContentType()==null||request.getContentType().startsWith("multipart/form-data")){
-        response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE,"Invalid content type or no content type is provided");
-        return;
-    }
+        if (request.getContentType() == null || request.getContentType().startsWith("multipart/form-data")) {
+            response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Invalid content type or no content type is provided");
+            return;
+        }
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        Part picture = request.getPart("picture");
+
+        if (name == null || !name.matches("[A-Za-z ]+")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid Name or name not provided");
+        } else if (email == null || !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid Email or email not provided");
+        } else if (password == null || password.trim().isEmpty()) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid password, or password cannot be empty");
+        } else if (picture != null && !picture.getContentType().startsWith("image/")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid Image type");
+        }
 
     }
 }
