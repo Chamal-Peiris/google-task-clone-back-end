14b444d86a4ef4df1a3d97c0b431ee1fb2563a34
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-20 13:50:42 +0530

    Finalize the doPost() method for task list

--- src/main/java/lk/ijse/dep8/tasks/api/TaskListServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/TaskListServlet.java
@@ -1,6 +1,10 @@
 package lk.ijse.dep8.tasks.api;
 
 import com.mysql.cj.log.Log;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
+import lk.ijse.dep8.tasks.dto.TaskListDTO;
 import lk.ijse.dep8.tasks.util.HttpServlet2;
 import lk.ijse.dep8.tasks.util.ResponseStatusException;
 
@@ -12,10 +16,7 @@ import javax.servlet.annotation.*;
 import javax.sql.DataSource;
 import javax.xml.crypto.Data;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -46,34 +47,53 @@ private final Logger logger= Logger.getLogger(TaskListServlet.class.getName());
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        if (request.getContentType() == null || !request.getContentType().startsWith("application/json")) {
+        if (req.getContentType() == null || !req.getContentType().startsWith("application/json")) {
             throw new ResponseStatusException(415, "Invalid content type or content type is empty");
         }
 
         String pattern = "/([A-Fa-f0-9\\-]{36})/lists/?";
-        if (!request.getPathInfo().matches(pattern)) {
+        if (!req.getPathInfo().matches(pattern)) {
             throw new ResponseStatusException(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Invalid end point for post request");
         }
-        Matcher matcher = Pattern.compile(pattern).matcher(request.getPathInfo());
+        Matcher matcher = Pattern.compile(pattern).matcher(req.getPathInfo());
         matcher.find();
         String userId = matcher.group(1);
 
-        try(Connection connection = pool.get().getConnection()){
+        try (Connection connection = pool.get().getConnection()) {
             PreparedStatement stm = connection.prepareStatement("SELECT * FROM user WHERE id=?");
-            stm.setString(1,userId);
+            stm.setString(1, userId);
             ResultSet rst = stm.executeQuery();
-            if(!rst.next()){
-                throw new  ResponseStatusException(404,"Invalid user id");
-            }else{
-
+            if (!rst.next()) {
+                throw new ResponseStatusException(404, "Invalid user id");
             }
 
+            Jsonb jsonb = JsonbBuilder.create();
+            TaskListDTO taskList = jsonb.fromJson(req.getReader(), TaskListDTO.class);
 
+            if (taskList.getTitle().trim().isEmpty()) {
+                throw new ResponseStatusException(400, "Invalid title or title is empty");
+            }
 
+            stm = connection.prepareStatement("INSERT INTO task_list (name,user_id) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, taskList.getTitle());
+            stm.setString(2, userId);
+            if (stm.executeUpdate() != 1) {
+                throw new SQLException("Failed to save the task list");
+            }
+
+            rst = stm.getGeneratedKeys();
+            rst.next();
+            taskList.setId(rst.getInt(1));
+
+            resp.setContentType("application/json");
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            jsonb.toJson(taskList, resp.getWriter());
+        } catch (JsonbException e) {
+            throw new ResponseStatusException(400, "Invalid JSON", e);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new ResponseStatusException(500, e.getMessage(), e);
         }
 
 
