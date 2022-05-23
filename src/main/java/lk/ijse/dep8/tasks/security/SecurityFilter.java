d6a2f336af19a399cb9fc27da33c61a905904504
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-23 12:28:50 +0530

    Add a bsic filter to get the base 64

--- src/main/java/lk/ijse/dep8/tasks/security/SecurityFilter.java
+++ src/main/java/lk/ijse/dep8/tasks/security/SecurityFilter.java
@@ -1,8 +1,41 @@
 package lk.ijse.dep8.tasks.security;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
-@WebFilter
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Base64;
+
+@WebFilter(filterName = "SecurityFilter", urlPatterns = "/*")
 public class SecurityFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        String appContextPath = req.getContextPath();
+
+        if (req.getRequestURI().matches(appContextPath + "/v1/users/?") && req.getMethod().equals("POST")) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        String authorization = req.getHeader("Authorization");
+
+        if (authorization == null || !authorization.startsWith("Basic")) {
+            res.setStatus(403);
+        }
+        String base64Credentials = authorization.replaceAll("Basic", "");
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] decode = decoder.decode(base64Credentials);
+
+
+
+
+    }
+
 
 }
