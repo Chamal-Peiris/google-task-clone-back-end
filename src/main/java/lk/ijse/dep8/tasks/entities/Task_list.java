be24bf5dd7602f4d4a535c9198e8841294c9457d
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-24 20:56:16 +0530

    Add Lombok to the project

--- src/main/java/lk/ijse/dep8/tasks/entities/Task_list.java
+++ src/main/java/lk/ijse/dep8/tasks/entities/Task_list.java
@@ -1,42 +1,16 @@
 package lk.ijse.dep8.tasks.entities;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Task_list implements Serializable {
     int id;
     String name;
     String userId;
 
-    public Task_list() {
-    }
-
-    public Task_list(int id, String name, String userId) {
-        this.id = id;
-        this.name = name;
-        this.userId = userId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 }
--- src/main/java/lk/ijse/dep8/tasks/entities/User.java
+++ src/main/java/lk/ijse/dep8/tasks/entities/User.java
@@ -1,7 +1,13 @@
 package lk.ijse.dep8.tasks.entities;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User implements Serializable {
     String id;
     String email;
@@ -9,54 +15,5 @@ public class User implements Serializable {
     String fullName;
     String profilePic;
 
-    public User() {
-    }
 
-    public User(String id, String email, String password, String fullName, String profilePic) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.fullName = fullName;
-        this.profilePic = profilePic;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getProfilePic() {
-        return profilePic;
-    }
-
-    public void setProfilePic(String profilePic) {
-        this.profilePic = profilePic;
-    }
 }
