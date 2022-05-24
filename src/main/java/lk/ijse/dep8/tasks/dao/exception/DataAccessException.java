48552ef6ac8eb4c0411ea750504846f516a26a44
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-24 21:39:02 +0530

    Add UserDAO and add DataAccessException Class

---
+++ src/main/java/lk/ijse/dep8/tasks/dao/exception/DataAccessException.java
@@ -0,0 +1,11 @@
+package lk.ijse.dep8.tasks.dao.exception;
+
+public class DataAccessException  extends RuntimeException{
+    public DataAccessException() {
+        super();
+    }
+
+    public DataAccessException(String message) {
+        super(message);
+    }
+}
