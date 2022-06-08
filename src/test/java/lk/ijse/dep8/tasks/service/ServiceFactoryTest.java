281efcce7187bd1714da17d3f8158fe31d5f3a05
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-08 13:37:43 +0530

    Finalize hibernate util and session factory

--- src/test/java/lk/ijse/dep8/tasks/service/ServiceFactoryTest.java
+++ src/test/java/lk/ijse/dep8/tasks/service/ServiceFactoryTest.java
@@ -9,18 +9,5 @@ import static org.mockito.Mockito.mock;
 
 class ServiceFactoryTest {
 
-    @Test
-    void getInstance() {
-        ServiceFactory isntance1 = ServiceFactory.getInstance();
-        ServiceFactory isntance2 = ServiceFactory.getInstance();
-        assertEquals(isntance1,isntance2);
-    }
 
-    @Test
-    void getService() {
-        Connection mockConnetion=mock(Connection.class);
-        SuperSevice userService=ServiceFactory.getInstance().getService(mockConnetion, ServiceFactory.ServiceTypes.USER);
-        SuperSevice taskService=ServiceFactory.getInstance().getService(mockConnetion, ServiceFactory.ServiceTypes.TASK);
-
-    }
 }
---
+++ src/test/java/lk/ijse/dep8/tasks/service/util/HibernateUtilTest.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep8.tasks.service.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HibernateUtilTest {
+
+    @Test
+    void getSessionFactory() {
+        //given
+
+        //when
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+
+        //then
+        assertNotNull(sf);
+        assertNotNull(session);
+
+
+    }
+}
